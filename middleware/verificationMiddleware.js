cd933ee3229f619e43026ca3c3a4f270e127145c
Riccio <riccio.fernando@gmail.com>
2025-04-20 23:28:39 -0300

    moved password/email check to /register only

--- middleware/verificationMiddleware.js
+++ middleware/verificationMiddleware.js
@@ -1,13 +1,8 @@
-const passwordRegex = /^(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?`~]).{5,}$/
-const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 export default function verificationMiddleware(req, res, next) {
     let username = req.body?.username;
     let password = req.body?.password
     if (!username) return res.sendStatus(400);
     if (!password) return res.sendStatus(400);
-
-    if(!emailRegex.test(username)) return res.sendStatus(400).json({ message: 'Email inválido' });
-    if(!passwordRegex.test(password)) return res.sendStatus(400).json({ message: 'Senha deve conter no mínimo 5 dígitos, uma letra maiúscula, um número e um caractere especial' });
 
     next();
 }
