8a9d90fc2e05fc3a7c3801eb590b6b3e714d64b1
Riccio <166852556+Ricciow@users.noreply.github.com>
2025-04-25 19:18:59 -0300

    Modify Token

--- routes/auth.js
+++ routes/auth.js
@@ -15,38 +15,40 @@ const passwordRegex = /^(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_\-+=\[\]{};':"\\|,.<
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 router.post('/register', verificationMiddleware, async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, type } = req.body;
 
-    const userExists = (await pool.query('SELECT * FROM users WHERE username = $1', [username])).rows[0];
+    const userExists = (await pool.query('SELECT * FROM users WHERE username = $1', [username])).rowCount > 0;
+
     if (userExists) return res.status(400).json({ message: 'Usuário já existe' });
 
     if(!emailRegex.test(username)) return res.status(400).json({ message: 'Email inválido' });
     if(!passwordRegex.test(password)) return res.status(400).json({ message: 'Senha deve conter no mínimo 5 dígitos, uma letra maiúscula, um número e um caractere especial' });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
+    await pool.query('INSERT INTO users (username, password, type) VALUES ($1, $2, $3)', [username, hashedPassword, type]);
 
     res.status(201).json({ message: 'Usuário criado com sucesso' });
 });
 
 router.post('/login', verificationMiddleware, async (req, res) => {
-    const { username, password, remindMe } = req.body;
+    const { username, password } = req.body;
 
     const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
     const user = result.rows[0];
+
     if (!user) return res.status(401).json({ message: 'Usuário ou senha incorreta' });
 
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) return res.status(401).json({ message: 'Usuário ou senha incorreta' });
 
-    //Token deve durar 24 Horas por padrão, se remindMe for true, ele não expira
-    const token = jwt.sign({ username }, SECRET, remindMe ? undefined : { expiresIn: '24h' });
+    const token = jwt.sign({ username: user.username, type: user.type }, SECRET, { expiresIn: '1h' });
     res.json({ token });
 });
 
 router.get('/key', authMiddleware, (req, res) => {
     res.json({
-        key: req.user.username,
+        username: req.user.username,
+        type: req.user.type,
         expiresIn: req.user.exp 
     });
 });
